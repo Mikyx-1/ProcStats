507fccfa84ccc2902722007b544523c29b7990e6
le_hoang_viet <le_hoang_viet@monorevo.jp>
2025-05-31 23:26:37 +0700

    update __init__.py and pyproject.toml files

--- src/procstats/__init__.py
+++ src/procstats/__init__.py
@@ -1,5 +1,3 @@
 from .combined_procstats import combined_resource_monitor
-from .cpu_ram_monitoring import cpu_resource_monitor
-from .gpu_monitoring import gpu_resource_monitor
 
 __version__ = "0.1.0"
